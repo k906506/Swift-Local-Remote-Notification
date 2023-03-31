b449d3f4a75cfcb0fb1b42f609baddaf4a35fabe
k906506 <k906506@naver.com>
2023-03-31 13:43:30 +0900

    프로젝트 초기 세팅

---
+++ Local-Remote-Notification/Local-Remote-Notification/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Local-Remote-Notification
+//
+//  Created by 고도현 on 2023/03/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Local-Remote-Notification/Local-Remote-Notification/Local_Remote_NotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Local_Remote_NotificationApp.swift
+//  Local-Remote-Notification
+//
+//  Created by 고도현 on 2023/03/31.
+//
+
+import SwiftUI
+
+@main
+struct Local_Remote_NotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
